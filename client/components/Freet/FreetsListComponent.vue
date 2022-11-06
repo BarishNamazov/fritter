aea968fa18e34c03d66ade978ee2e9473b1af148
Abutalib (Barish) Namazov <barish2003@gmail.com>
2022-11-06 17:48:32 -0500

    refactoring and adding feed

---
+++ client/components/Freet/FreetsListComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <section v-if="freets.length">
+    <FreetComponent
+      v-for="freet in freets"
+      :key="freet.id"
+      :freet="freet"
+    />
+  </section>
+  <article v-else>
+    <h3>No freets found.</h3>
+  </article>
+</template>
+
+<script>
+import FreetComponent from "@/components/Freet/FreetComponent.vue";
+
+export default {
+  name: "FreetsListComponent",
+  components: { FreetComponent },
+  props: {
+    freets: {
+      type: Array,
+      required: true
+    }
+  },
+};
+</script>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+}
+</style>
