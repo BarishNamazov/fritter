b8547b5676bc7658e8e95ce4309192585a46c47c
Abutalib (Barish) Namazov <barish2003@gmail.com>
2022-11-07 04:03:02 -0500

    friend request styling

--- client/components/Friend/FriendRequestsListPage.vue
+++ client/components/Friend/FriendRequestsListPage.vue
@@ -1,23 +1,24 @@
 <template>
   <main>
     <h1>Friend Requests</h1>
-    <button @click="type = 'incoming'">Incoming ({{ countIncoming }})</button>
-    <button @click="type = 'outgoing'">Outgoing ({{ countOutgoing }})</button>
-    <input type="checkbox" v-model="showAll" id="showAll" />
-    <label for="showAll">Show completed requests</label>
+    <span class="button-container">
+      <button @click="type = 'incoming'">Incoming ({{ countIncoming }})</button>
+      <button @click="type = 'outgoing'">Outgoing ({{ countOutgoing }})</button>
+      <label><input type="checkbox" v-model="showAll" />Show completed requests</label>
+    </span>
     <article>
       <h2 v-if="type === 'incoming'">Incoming requests</h2>
       <h2 v-else>Outgoing requests</h2>
       <ul>
         <div v-for="request in requests" :key="request.id">
           <li v-if="request.type === type && request.status === 'pending'">
-            <UsernameComponent :username="request.username" />
+            <UsernameComponent class="username" :username="request.username" />
             <button v-if="type === 'incoming'" @click="respondRequest(request.username, 'accept')">Accept</button>
             <button v-if="type === 'incoming'" @click="respondRequest(request.username, 'reject')">Reject</button>
             <button v-if="type === 'outgoing'" @click="cancelRequest(request.username)">Cancel</button>
           </li>
           <li v-else-if="request.type === type && showAll">
-            <UsernameComponent :username="request.username" />
+            <UsernameComponent class="username" :username="request.username" />
             <span v-if="request.status === 'accepted'">Accepted</span>
             <span v-else-if="request.status === 'rejected'">Rejected</span>
           </li>
@@ -71,7 +72,6 @@ export default {
               status: request.status
             };
           });
-          console.log(this.requests);
         });
     },
     respondRequest(username, response) {
@@ -102,3 +102,31 @@ export default {
 };
 
 </script>
+
+<style scoped>
+main {
+  padding-top: 1em;
+}
+
+li {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border: 1px solid var(--username-border-color);
+  border-radius: var(--border-radius-large);
+  padding: 1em;
+  gap: 1em;
+  margin-bottom: 1em;
+}
+
+.username {
+  margin-right: auto;
+}
+
+.button-container {
+  display: flex;
+  gap: 1em;
+  margin-bottom: 1em;
+  align-items: center;
+}
+</style>
